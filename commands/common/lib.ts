9908e970acda57a7d14d468008da1a8e3e3c3752
LemonPrefect <me@lemonprefect.cn>
2022-12-14 00:26:45 +0800

    Optimization(import lib): clean code.

--- commands/common/lib.ts
+++ commands/common/lib.ts
@@ -1,35 +1,19 @@
 /** deno */
-import axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
 import os from "https://deno.land/x/dos@v0.11.0/mod.ts";
-import { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
 import * as progress from "https://deno.land/x/progress@v1.3.4/mod.ts";
 export {
-	axiod,
 	os,
-	hmac,
 	progress
 };
 
 /** deno std */
 import * as path from "https://deno.land/std@0.110.0/path/mod.ts";
 import * as fs from "https://deno.land/std@0.167.0/node/fs.ts";
-import { Buffer } from "https://deno.land/std@0.167.0/node/buffer.ts";
-import { iterateReader } from "https://deno.land/std@0.162.0/streams/conversion.ts";
 import { walk } from "https://deno.land/std@0.121.0/fs/walk.ts";
 export {
 	path,
 	fs,
-	Buffer,
-	iterateReader,
 	walk
-};
-
-/** deno base64 */
-import * as base64url from "https://denopkg.com/chiefbiiko/base64@master/base64url.ts";
-import * as base64 from "https://denopkg.com/chiefbiiko/base64@master/mod.ts";
-export {
-	base64url,
-	base64
 };
 
 /** deno cliffy */
@@ -46,20 +30,4 @@ export {
 	colors,
 	tty,
 	ansi
-};
-
-/** types axiod */
-import { IAxiodResponse } from "https://deno.land/x/axiod@0.26.2/interfaces.ts";
-export type {
-	IAxiodResponse
-};
-
-/** npms */
-import * as yaml from "npm:yaml@2.1.3";
-import COS from "npm:cos-nodejs-sdk-v5@2.11.18/index.js";
-import * as crc64 from "npm:crc64-ecma182.js@1.0.0/crc64_ecma182.js";
-export {
-	yaml,
-	COS,
-	crc64
 };
